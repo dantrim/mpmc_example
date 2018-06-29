58562ab553da019a23d6f9d41d5ca82b9cc54f1c
Daniel Joseph Antrim <daniel.joseph.antrim@cern.ch>
2018-06-29 13:25:49 +0200

    header file with typedefs for various maps

---
+++ include/map_defs.h
@@ -0,0 +1,20 @@
+#ifndef MPMC_MAP_DEFS_H
+#define MPMC_MAP_DEFS_H
+
+// moodycamel
+#include "concurrentqueue/concurrentqueue.h"
+using moodycamel::ConcurrentQueue;
+
+// junction
+#include <junction/ConcurrentMap_Grampa.h>
+#include <junction/details/Grampa.h>
+#include <turf/Heap.h>
+
+// libcuckoo
+#include <libcuckoo/cuckoohash_map.hh>
+
+typedef junction::ConcurrentMap_Grampa<turf::u32, ConcurrentQueue<DataFragment>* > L1IndexMap;
+
+typedef cuckoohash_map<uint32_t, ConcurrentQueue<DataFragment>*> L1IndexHash;
+
+#endif
